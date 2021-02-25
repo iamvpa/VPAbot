4703f335f1f3d72747a2222fd30a004742390b78
Vishnu Priy Agnihotri <vishnu.csebt18@nituk.ac.in>
2021-02-25 09:02:12 +0000

    added keep_alive

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import json
 import random
 from replit import db
+from keep_alive import keep_alive
 
 client=discord.Client()
 
@@ -107,4 +108,6 @@ async def on_message(message):
     else:
       db["responding"]=False
       await message.channel.send("Responding is off.")
+
+keep_alive()
 client.run(os.getenv('TOKEN'))
