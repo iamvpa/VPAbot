6e7a2abcaac793939c03605c05c18ca77a95d40e
Vishnu Priy Agnihotri <vishnu.csebt18@nituk.ac.in>
2021-02-25 07:02:47 +0000

    added quotes

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ client=discord.Client()
 def get_quote():
   response=requests.get("https://zenquotes.io/api/random")
   json_data=json.loads(response.text)
+  quote=json_data[0]['q'] + " -" + json_data[0]['a']
+  return(quote)
+
+
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'
@@ -18,7 +22,8 @@ async def on_message(message):
   if message.author == client.user: 
     return
 
-  if message.content.startswith('vpa'):
-    await message.channel.send('Hello!')
+  if message.content.startswith('vpaInspire'):
+    quote=get_quote
+    await message.channel.send(quote)
 
 client.run(os.getenv('TOKEN'))
